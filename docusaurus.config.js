1b674745169874a1de1caa91433e6c0720333432
onmyoji0712 <257896687@qq.com>
2023-01-10 11:43:13 +0800

    Signed-off-by: onmyoji0712 <257896687@qq.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,8 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
+  deploymentBranch: 'gh-pages',
+  plugins: ['@docusaurus/theme-live-codeblock'],
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
